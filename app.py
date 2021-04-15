86c0fc8bc0a9d615cb36ec0f706e175048a77f03
ahmed hassan <50107057+ahmed-hassan97@users.noreply.github.com>
2021-04-15 15:42:09 +0200

    Update app.py
    modify code

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# %%writefile app.py
 
 from tensorflow.keras.models import load_model  
 import streamlit as st 
@@ -6,9 +5,13 @@ import numpy as np
 import pandas as pd
 import base64
 
-first_model = "first_model.h5"
-second_model = "second_model.h5"
-final_model = "final_model.h5"
+# first_model = "first_model.h5"
+# second_model = "second_model.h5"
+# final_model = "final_model.h5"
+
+first_model = ""
+second_model = ""
+final_model = ""
 
 
 
@@ -53,7 +56,6 @@ def home_page():
     .reportview-container {
         background: url("https://www.xavor.com/wp-content/uploads/3-Intelligent-Manufacturing.jpg")
     }
-
     </style>
     """,
     unsafe_allow_html=True
@@ -67,7 +69,6 @@ def Telemetry_Electronics():
     .reportview-container {
         background: url("https://th.bing.com/th/id/R3c363a11aecebd7e76f18f23f81e5c2e?rik=xBXL%2bdFNrjtLSA&riu=http%3a%2f%2fwallpapercave.com%2fwp%2fCy7YPNg.jpg&ehk=QtoG1zotzCDq%2beIQ7SHA6xKvIQZaZGm13fK7HoOLxXA%3d&risl=&pid=ImgRaw")
     }
-
     </style>
     """,
     unsafe_allow_html=True
@@ -75,32 +76,32 @@ def Telemetry_Electronics():
 
     left_column , middle_column , right_column , last_column = st.beta_columns(4)
     # You can use a column just like st.sidebar:
-    left_column.button('Robot number' , key = 1)
+    left_column.button('choose Robot Name' , key = 1)
 
     # Or even better, call Streamlit functions inside a "with" block:
     with left_column:
         chosen_machine1 = st.radio(
-            'Robot number',
-            ("Robot A", "Robot B" ,"Robot C") , key = 2)
+            'Robot Name',
+            ("Circular Cutting Robot", "Straight cutting Robot" ,"Hole Cutting Robot") , key = 2)
         st.write(f"You Choose   {chosen_machine1} ")
 
 
-    middle_column.button('choose Robot number' , key = 3)
+    middle_column.button('choose Robot Name' , key = 3)
 
     # Or even better, call Streamlit functions inside a "with" block:
     with middle_column:
         chosen_machine2 = st.radio(
-            'Robot number',
-            ("Robot A", "Robot B" ,"Robot C") , key = 4)
+            'Robot Name',
+            ("Circular Cutting Robot", "Straight cutting Robot" , "hole Cutting Robot" ) , key = 4)
         st.write(f"You Choose   {chosen_machine2} ")        
 
 
-    right_column.button('choose Robot number' , key = 23)
+    right_column.button('choose Robot Name' , key = 23)
     # Or even better, call Streamlit functions inside a "with" block:
     with right_column:
         chosen_machine21 = st.radio(
-            'Robot number',
-            ("Robot A", "Robot B" ,"Robot C") , key = 24)
+            'Robot Name',
+            ("Circular Cutting Robot", "Straight cutting Robot" , "hole Cutting Robot" ) , key = 24)
         st.write(f"You Choose  {chosen_machine21} ") 
         
     last_column.button('choose connection Between Robot' ,key = 5)
@@ -108,9 +109,9 @@ def Telemetry_Electronics():
     with last_column:
 
         chosen_kind1 = st.radio(
-            'Robot number',
+            'Robot connection',
             ("Parallel" , ) , key = 14)
-        st.write(f"Robot connection {chosen_kind1} ")   
+        st.write(f"You Choose connection Between Robot {chosen_kind1} ")   
     st.markdown("<h1 style='text-align: right; color: black;'> Upload Features For The Robot in The Telemetry Electronics component </h1>", unsafe_allow_html=True)
     fileTypes = ["csv"]
     file = st.file_uploader("", type=fileTypes)
@@ -139,7 +140,6 @@ def Steering_Control():
     .reportview-container {
         background: url("https://th.bing.com/th/id/R3c363a11aecebd7e76f18f23f81e5c2e?rik=xBXL%2bdFNrjtLSA&riu=http%3a%2f%2fwallpapercave.com%2fwp%2fCy7YPNg.jpg&ehk=QtoG1zotzCDq%2beIQ7SHA6xKvIQZaZGm13fK7HoOLxXA%3d&risl=&pid=ImgRaw")
     }
-
     </style>
     """,
     unsafe_allow_html=True
@@ -148,35 +148,35 @@ def Steering_Control():
     left_column , middle_column , right_column = st.beta_columns(3)
     # You can use a column just like st.sidebar:
 
-    left_column.button('choose Robot number' , key = 11)
+    left_column.button('choose Robot Name' , key = 11)
 
     # Or even better, call Streamlit functions inside a "with" block:
     with left_column:
         
         chosen_machine3 = st.radio(
-            'Robot number',
-            ("Robot D", "Robot E") , key = 21)
+            'Robot Name',
+            ("Turret Lathe Robot", "Vertical Lathe Robot") , key = 21)
         st.write(f"You Choose  {chosen_machine3} ")
 
 
-    middle_column.button('choose Robot number' , key = 31)
+    middle_column.button('choose Robot Name' , key = 31)
 
     # Or even better, call Streamlit functions inside a "with" block:
     with middle_column:
         
         chosen_machine4 = st.radio(
-            'Robot number',
-            ( "Robot D", "Robot E") , key = 41)
+            'Robot Name',
+            ( "Turret Lathe Robot", "Vertical Lathe Robot") , key = 41)
         st.write(f"You Choose   {chosen_machine4} ")        
 
-    right_column.button('choose connection Between Robots' ,key = 51)
+    right_column.button('choose connection Between Robot' ,key = 51)
 
     # Or even better, call Streamlit functions inside a "with" block:
     with right_column:
         chosen_kind2 = st.radio(
-            'Robot number',
+            'Robot connection ',
             ("Series", "Parallel") , key =31 )
-        st.write(f"You Choose Robot {chosen_kind2} ")   
+        st.write(f"You Choose connection Between Robot {chosen_kind2} ")   
 
     st.markdown("<h1 style='text-align: right; color: black;'> Upload Features For The machines in The Steering Control component </h1>", unsafe_allow_html=True)
 
@@ -210,7 +210,6 @@ def Thrust_control():
     .reportview-container {
         background: url("https://th.bing.com/th/id/R3c363a11aecebd7e76f18f23f81e5c2e?rik=xBXL%2bdFNrjtLSA&riu=http%3a%2f%2fwallpapercave.com%2fwp%2fCy7YPNg.jpg&ehk=QtoG1zotzCDq%2beIQ7SHA6xKvIQZaZGm13fK7HoOLxXA%3d&risl=&pid=ImgRaw")
     }
-
     </style>
     """,
     unsafe_allow_html=True
@@ -225,28 +224,28 @@ def Thrust_control():
     with left_column:
         
         chosen_machine3 = st.radio(
-            'Robot number',
-            ("Robot F", "Robot G") , key = 21)
+            'Robot Name',
+            ("3D Cutting lisar Robot", "Strapping Robot") , key = 21)
         st.write(f"You Choose  {chosen_machine3} ")
 
 
-    middle_column.button('choose Robot number' , key = 301)
+    middle_column.button('choose Robot Name' , key = 301)
 
     # Or even better, call Streamlit functions inside a "with" block:
     with middle_column:
         chosen_machine4 = st.radio(
-            'Robot number',
-            ( "Robot F", "Robot G") , key = 41)
+            'Robot Name',
+            ( "3D Cutting lisar Robot", "Strapping Robot") , key = 41)
         st.write(f"You Choose {chosen_machine4} ")        
 
-    right_column.button('choose Robot number' ,key = 501)
+    right_column.button('choose Robot Name' ,key = 501)
 
     # Or even better, call Streamlit functions inside a "with" block:
     with right_column:
         chosen_kind2 = st.radio(
-            'Robot number',
+            'Robot connection ',
             ("Series", "Parallel") , key =331 )
-        st.write(f"You Choose Robot to be  {chosen_kind2} ")   
+        st.write(f"You Choose connection Between Robot  {chosen_kind2} ")   
 
     st.markdown("<h1 style='text-align: right; color: black;'> Upload Features For The machines in The Thrust control component </h1>", unsafe_allow_html=True)
 
@@ -272,6 +271,5 @@ def Thrust_control():
     
 
 
-if __name__ == "__main__":
-   
+if __name__ == "__main__": 
     main()
